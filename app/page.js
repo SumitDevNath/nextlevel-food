9251f96a46db4cb65629b18d68877752ede57150
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-08-16 17:59:35 +0600

    page routing

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
+      <main>
+        <p>
+          <Link href="/meals">meals</Link>
+        </p>
+        <p>
+          <Link href="/community">community</Link>
+        </p>
+        <p>
+          <Link href="/meals/share">share</Link>
+        </p>
+      </main>
     </main>
   );
 }
